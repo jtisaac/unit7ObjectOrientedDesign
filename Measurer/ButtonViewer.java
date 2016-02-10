8d66c7251e8246d224dc2ef3e5eb5b24a5226c29
jtisaac <jtisaac@NN123-HA-15.HS.sd203.org>
2016-02-10 15:07:35 -0600

    New buttonclicker!

--- Measurer/ButtonViewer.java
+++ Measurer/ButtonViewer.java
@@ -1,22 +1,63 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
-
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class ButtonViewer
 {
     private static final int FRAME_WIDTH = 100;
     private static final int FRAME_HEIGHT = 60;
-    
-    public static void main( String[] args )
+
+    private JFrame frame;
+    private JPanel panel;
+    private JButton button;
+    private JButton button2;
+    private JLabel label;
+    public ButtonViewer( )
     {
-        JFrame frame = new JFrame();
-        JButton button = new JButton( "Click Me!" );
-        frame.add( button );
-        
+        this.frame = new JFrame();
+        this.panel = new JPanel();
+
+        this.button = new JButton( "A" );
+        this.panel.add( this.button );
+
+        this.button2 = new JButton( "B" );
+        this.panel.add( this.button2 );
+
+        this.label = new JLabel();
+        this.panel.add(this.label);
+
+        this.frame.add(this.panel);
+
         ClickListener listener = new ClickListener();
-        button.addActionListener( listener );
-        
-        frame.setSize( FRAME_WIDTH, FRAME_HEIGHT );
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        frame.setVisible( true );
+        this.button.addActionListener( listener );
+
+        //ClickListener listener2 = new ClickListener("B");
+        this.button2.addActionListener( listener );
+
+        this.frame.setSize( FRAME_WIDTH, FRAME_HEIGHT );
+        this.frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        this.frame.setVisible( true );
+    }
+
+    public static void main(String[] args)
+    {
+        ButtonViewer view = new ButtonViewer();
+    }
+    public class ClickListener implements ActionListener
+    {
+        //private String name;
+
+        //public ClickListener(String buttonName)
+        //{
+        //  this.name = buttonName;
+        //}
+
+        public void actionPerformed( ActionEvent event )
+        {
+            label.setText("Button" + event.getActionCommand()+ "Was clicked");
+        }
     }
 }
