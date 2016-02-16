ba7997c47b8f7e381b3fe2fdcd8bd9a5a1b51e9a
jtisaac <jtisaac@NN123-HA-15.HS.sd203.org>
2016-02-16 15:07:39 -0600

    Starting the TRIANGLE VIEWER!<!

---
+++ Triangle/TriangleViewer.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import java.awt.Graphics2D;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComponent;
+/**
+This component displays a rectangle that can be moved.
+ */
+public class TriangleViewer extends JComponent
+{
+   public class ClickListener implements ActionListener
+   {
+   }
+    
+}
